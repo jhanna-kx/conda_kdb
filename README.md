d0014e7d0f0989660a9782805b4ca647407ecdc1
James Hanna <jhanna@kx.com>
2018-11-04 15:36:37 +0000

    wrapper scripts

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # conda_kdb
 conda_kdb
 Build with 
+put l64/m64/w64.zip in this directory then run `conda_build.sh` or `conda_build.bat` on windows (7zip required)
 ```
 export LICDIR=LICENCEDIRECTORY
+export 
 conda build --no-long-test-prefix --no-include-recipe .
 ```
---
+++ conda_build.bat
@@ -0,0 +1,54 @@
+#!/bin/bash
+function licexist(){
+ if [ -f $1/kc.lic ] || [ -f $1/k4.lic ]
+ then
+  export LICDIR=$1
+ else
+  echo "k4.lic/kc.lic not found in: $1" >&2
+  exit 1
+ fi
+ }
+if [ "x" != "x$LICDIR" ]
+then
+ licexist $LICDIR
+else
+ if [ "x" != "x$QLIC" ]
+ then
+  licexist $QLIC
+ elif [ "x" != "x$QHOME" ]
+ then
+  licexist $QHOME
+ else
+  licexist .
+ fi
+fi
+if [ $(uname) == "Linux" ]
+then 
+ OSL=l
+elif [ $(uname) == "Darwin" ]
+then
+ OSL=m
+else
+ echo "unknown os $(uname)" >&2
+ exit 1
+fi
+
+KDBZIP=${OSL}64.zip
+if [ ! -f $KDBZIP ]
+then
+ echo "kdb zip file $KDBZIP not found" >&2
+ exit 1
+fi
+unzip -o $KDBZIP
+echo $LICDIR 
+export QHOME=$(pwd)
+export QLIC=$LICDIR
+QBUILD=$(echo .z.k|${OSL}64/q -q)
+export QBUILD
+
+conda build --no-long-test-prefix --no-include-recipe .
+
+
+
+
+   
